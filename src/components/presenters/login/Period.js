7b1f5960f3e0fde3b04844cfb714e784a5367c54
LIMHUIWON <59960678+H2-won@users.noreply.github.com>
2021-10-01 17:37:07 +0900

    update login / Add manage login

--- src/components/presenters/login/Period.js
+++ src/components/presenters/login/Period.js
@@ -1,14 +1,13 @@
-import React from 'react';
 import styled from 'styled-components';
 import { palette } from 'lib/styles';
+
+const Period = () => {
+  return <Term>October 19-22</Term>;
+};
 
 const Term = styled.h2`
   font-size: 18px;
   color: ${palette.white};
 `;
-
-function Period() {
-  return <Term>October 19-22</Term>;
-}
 
 export default Period;
