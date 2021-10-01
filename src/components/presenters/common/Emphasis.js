51a2c145ac6a96c97f7886cbd170d44525efb0e5
NB <altmshfkgudtjr@naver.com>
2021-10-01 14:13:31 +0900

    Create Emphasis.js

---
+++ src/components/presenters/common/Emphasis.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+// styles
+import { palette } from 'lib/styles';
+
+/**
+ * @example
+ * <Emphasis>
+ *   일반구문
+ *   <br />
+ *   <strong>강조구문</strong>
+ * </Emphasis>
+ */
+const Emphahsis = ({ children }) => {
+  return <Message>{children}</Message>;
+};
+
+const Message = styled.div`
+	text-align: center;
+	color: ${palette.font.secondary};
+	font-size: 36px;
+	font-weight: bold;
+	line-height: 60px;
+
+	& > strong {
+		color: ${palette.white}
+		font-size: inherit;
+		font-weight: inherit;
+		line-height: inherit;
+	}
+`;
+
+export default Emphahsis;
